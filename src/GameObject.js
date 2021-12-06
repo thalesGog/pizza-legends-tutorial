c13b02494f4ec0f008050cbf7e660c38ab21e0ff
Thales Nunes <thales_nunes@sicredi.com.br>
2021-12-06 17:58:52 -0300

    add tutorial part 4 code

--- src/GameObject.js
+++ src/GameObject.js
@@ -5,11 +5,14 @@ class GameObject {
   constructor(config) {
     this.x = config.x || 0;
     this.y = config.y || 0;
+    this.direction = config.direction || "down";
     this.sprite = new Sprite({
       gameObject: this,
       src: config.src || hero,
     });
   }
+
+  update() {}
 }
 
 export default GameObject;
---
+++ src/utils.js
@@ -0,0 +1,1 @@
+export const withGrid = (n) => n * 16;
